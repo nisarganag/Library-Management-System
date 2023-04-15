89aef3ec6280ea87aa94cceb0930499caa5fb9e1
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2023-04-15 22:40:22 +0530

    latest

--- src/Library/ConnectionClass.java
+++ src/Library/ConnectionClass.java
@@ -1,13 +1,25 @@
 package Library;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.*;
 public class ConnectionClass {
     Connection con;
     Statement stm;
     ConnectionClass(){
+      
         try {
+            File file = new File("username.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String user=br.readLine();
+            File file2 = new File("password.txt");
+            BufferedReader br2 = new BufferedReader(new FileReader(file2));
+            String pass=br2.readLine();
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Library","root","JAMESbond007.");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Library",user,pass);
             stm=con.createStatement();
+            br.close();
+            br2.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
